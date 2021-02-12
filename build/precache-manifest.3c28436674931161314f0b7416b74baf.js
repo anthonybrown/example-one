d81591f52770fb706da20f7f2f2a878c5434ef4c
Tony Brown <tony@tonybrown.me>
2021-02-12 17:22:28 -0500

    set up for gh-pages

---
+++ build/precache-manifest.3c28436674931161314f0b7416b74baf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ac163d412c2999c012a58954eb1d619",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f02b8cb0d13d8bff385",
+    "url": "/static/css/main.6d3702d2.chunk.css"
+  },
+  {
+    "revision": "e0e7341c079fcfd2db9c",
+    "url": "/static/js/2.2599ee3c.chunk.js"
+  },
+  {
+    "revision": "3f02b8cb0d13d8bff385",
+    "url": "/static/js/main.ef835720.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
