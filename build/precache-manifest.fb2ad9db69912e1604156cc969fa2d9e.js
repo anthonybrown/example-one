75fb957e8635f2ae976ac1ae36b4d83242416f0f
Tony Brown <tony@tonybrown.me>
2021-02-12 17:57:31 -0500

    setup for github pages

---
+++ build/precache-manifest.fb2ad9db69912e1604156cc969fa2d9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "594bc991524a2183e9e4977dcef9277c",
+    "url": "/example-one/index.html"
+  },
+  {
+    "revision": "3f02b8cb0d13d8bff385",
+    "url": "/example-one/static/css/main.6d3702d2.chunk.css"
+  },
+  {
+    "revision": "e0e7341c079fcfd2db9c",
+    "url": "/example-one/static/js/2.2599ee3c.chunk.js"
+  },
+  {
+    "revision": "3f02b8cb0d13d8bff385",
+    "url": "/example-one/static/js/main.ef835720.chunk.js"
+  },
+  {
+    "revision": "e729f784d14a45a55c76",
+    "url": "/example-one/static/js/runtime~main.d305c888.js"
+  }
+]);
